71b1f1778e224a9f597d3bdf344d35ca8a9cb718
KeertanaG <keertana.govte@yesgnome.com>
2022-04-05 18:39:41 +0530

    FPS movement intialized. Enemy initialized. Spawn points are intialized.

---
+++ Assets/PlayerSpawn.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public Transform playerSpawnPoints; //Parent of the spawn points
+    Transform[] spawnPositions;
+    public bool isRespawn = false;
+    public bool respawnToggle = false;
+    int k;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPositions = playerSpawnPoints.GetComponentsInChildren<Transform>();
+       // Debug.Log(spawnPositions.Length);
+      //  RandomSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // int i = Random.Range(1, spawnPositions.Length);
+        /*  if(respawnToggle!=isRespawn)
+          {
+              RandomSpawn();
+              isRespawn = false;
+          }
+          else
+          {
+              respawnToggle = isRespawn;
+          }*/
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+          
+              k =RandomSpawn();
+        }
+        this.transform.position = spawnPositions[k].transform.position;
+
+    }
+    public int RandomSpawn()
+    {
+        int i = Random.Range(1, spawnPositions.Length);
+       Debug.Log(i);
+        this.transform.position = spawnPositions[i].transform.position;
+        return (i);
+    }
+}
